bcd931589ebf03bdf972c45dcac37b1b5e75ec28
Brian Bycynski <high5bri@hotmail.com>
2016-11-29 14:05:28 -0500

    "Hello, App App!"

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import 'file!./index.html'
+import './styles/screen.sass'
+
+const main = () => {
+  document.querySelector('h1').textContent += '!!'
+}
+
+document.addEventListener('DOMContentLoaded', main)
+
+if (module.hot) {
+  module.hot.accept()
+  module.hot.accept('file!./index.html', () => {
+    window.location.reload()
+  })
+}
